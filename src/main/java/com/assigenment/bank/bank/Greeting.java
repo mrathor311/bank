effd71ee79f3f677c54f43eb120ffd5bb615fa9f
mrathor311 <mrathor311@gmail.com>
2024-12-07 21:42:06 +0530

    greeting end point created

---
+++ src/main/java/com/assigenment/bank/bank/Greeting.java
@@ -0,0 +1,14 @@
+package com.assigenment.bank.bank;
+
+public record Greeting(long id,String content){}
+
+/* {
+    long id;
+    String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+}*/
