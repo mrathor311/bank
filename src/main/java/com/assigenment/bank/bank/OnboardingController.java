f885156f8228184d5fcfbc9bfa281e64877a2525
mrathor311 <mrathor311@gmail.com>
2024-12-15 14:51:05 +0530

    login post api created

--- src/main/java/com/assigenment/bank/bank/OnboardingController.java
+++ src/main/java/com/assigenment/bank/bank/OnboardingController.java
@@ -1,8 +1,6 @@
 package com.assigenment.bank.bank;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -13,11 +11,40 @@ public class OnboardingController {
     private final AtomicLong counter = new AtomicLong();
     @GetMapping("/greeting")
 public Greeting greeting(
-        @RequestParam(value = "rahul")
+        @RequestParam(value = "name")
        String name
-){
+)
+    {
 return new Greeting(counter.incrementAndGet(),String.format("my name is rahulsaxsena "+ template, name,"Muskesh",6));
 }
+@PostMapping("/login")
+    public  LoginResponse loginUser(@RequestBody User user){
 
+      String email  =user.email();
+       String pass = user.pass();
+        if ( email== null|| email.isEmpty())
+            return  new LoginResponse(0,"email id is invlid",null) ;
+        if ( pass==null|| pass.isEmpty())
+            return new LoginResponse(0," pass in valid",null);
 
+        // database valid entry
+    final String name = "ss1@getnada.com";
+    final String passw="Aa123456@";
+    if (email.equals(name) && pass.equals(passw)){
+        return new LoginResponse(1,"success",
+                new Login("mukesh","8107776311","ss1@getnada.com"));
+    }
+    return new LoginResponse(0,"invelid user id and password",null);
+
+    }
+public record User( String email, String pass){
+
+}
+public  record  LoginResponse(int Status,String Message, Login user
+                              ){
+
+}
+public  record Login(String name,String Phone,String email){
+
+}
 }
